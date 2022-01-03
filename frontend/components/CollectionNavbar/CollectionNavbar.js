62952d095cbc2630d45eb266c50d4d898bf2516e
omrubiks <om.rubiks@gmail.com>
2022-01-03 09:47:27 +0530

    Footer Done

---
+++ frontend/components/CollectionNavbar/CollectionNavbar.js
@@ -0,0 +1,34 @@
+/*
+Collection List:
+CPU
+CPU Cooler
+MotherBoard
+Memory
+Storage
+Video Card
+Case
+Power Supply
+Operating System
+Monitor
+*/
+
+const CollectionNavbar = ()=>{
+    return(
+        <div>
+            <ul className="flex justify-evenly md:block bg-red-200">
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">CPU</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Mother Board</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">CPU Cooler</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Memory</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Storage</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Video Card</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Case</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Power Supply</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Opearting System</li>
+                <li className="p-2 hover:bg-red-500  transition ease-in-out  duration-500">Monitor</li>
+            </ul>
+        </div>
+    )
+};
+
+export default CollectionNavbar;
