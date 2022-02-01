c3a98cd12782ff014c572b577c499c253c674f70
omrubiks <om.rubiks@gmail.com>
2022-02-01 18:22:46 +0530

    Some Responsive Changes

--- frontend/components/Cart/CartData.js
+++ frontend/components/Cart/CartData.js
@@ -2,12 +2,6 @@ import Link from "next/link";
 
 import ShoppingProductCard from "./ShoppingProductCard";
 import Checkout from "./Checkout";
-// id: 1,
-// name: "Core i9",
-// price: "70000",
-// comparedPrice: "90000",
-// category: "CPU",
-// image: "http
 
 const CartData = (props) => {
   return (
@@ -15,8 +9,8 @@ const CartData = (props) => {
       <div className="p-2">
         <h1 className="text-5xl font-bold">Shopping Cart</h1>
       </div>
-      <div className="grid grid-cols-3 gap-8 mt-4">
-        <section className="col-span-2">
+      <div className="grid grid-cols-3 gap-8 mt-4 md:grid-cols-1 ">
+        <section className="col-span-2 ">
           <div>
             <h1 className="mb-5">3 Courses in Cart</h1>
             <div>
@@ -24,9 +18,9 @@ const CartData = (props) => {
                 {props.productslist.slice(0, 3).map((product) => (
                   <li key={product.id}>
                     <ShoppingProductCard
-                        name ={product.name}
-                        price = {product.price}
-                        image = {product.image}
+                      name={product.name}
+                      price={product.price}
+                      image={product.image}
                     />
                   </li>
                 ))}
@@ -35,7 +29,7 @@ const CartData = (props) => {
           </div>
         </section>
         <section>
-            <Checkout/>
+          <Checkout />
         </section>
       </div>
     </div>
