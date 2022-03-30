d5dfecf0a7e69a8a4bee5572e77e90aa60c384d8
Aman <amanlohani288@gmail.com>
2022-03-30 20:27:37 +0530

    Edited basic files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 const webpack = require("webpack");
+const withImages = require('next-images')
+
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  }
+})
 
 module.exports = {
   reactStrictMode: true,
