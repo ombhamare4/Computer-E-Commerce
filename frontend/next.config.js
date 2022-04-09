95e9fb3b2106038dcd2608dd26cef7fa63a57f93
Aman <amanlohani288@gmail.com>
2022-04-09 20:37:26 +0530

    M1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-const webpack = require("webpack");
-const withImages = require('next-images')
-
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-})
-
-=======
 const webpack = require('webpack');
->>>>>>> origin/frontend-component-testing
 module.exports = {
 webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 config.plugins.push(new webpack.ProvidePlugin({
